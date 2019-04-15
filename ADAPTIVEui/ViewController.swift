50954c174fddc0237566c009dbc4fd7de3e34c94
Makan Fofana <mfofanaT99@gmail.com>
2019-04-15 10:58:45 -0400

    Initial Commit

---
+++ ADAPTIVEui/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ADAPTIVEui
+//
+//  Created by Makan fofana on 4/15/19.
+//  Copyright © 2019 Makan fofana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
